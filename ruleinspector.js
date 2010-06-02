7d1cd0ba2192aef558cca5bdc7a8bf6bf605f9d5
helenadeus <helenadeus@gmail.com>
2010-06-02 10:18:14 +0100

    fixed

--- ruleinspector.js
+++ ruleinspector.js
@@ -1,5 +1,4 @@
 if(!self.get) include('get.js', 's3db_math');
-if(!self.s3dbcall) include('s3dbcall.js', 's3dbcall');
 //if(!self.S3QLtranslator) include('s3ql_translator.js','s3db_math');
 var s3dbUsers = {1:'Admin'};
 var missingSlots = [];var missingSlotIDS= [];
@@ -38,6 +37,7 @@ function ruleInpector(rules) {
 	}
 	var tbody = $(document.createElement('tbody'));
 	$('#table_rules').append(tbody);
+	
 	if(typeof(rules.error_code)==='undefined')
 	for (var i=0; i<rules.length; i++) {
 		var classColor = ((i%2)?'even':'odd');
@@ -88,23 +88,27 @@ function newRuleTable() {
 	
 	var tbody = $(document.createElement('tbody')).appendTo($('#table_new_rules'));
 	var inputTr = $(document.createElement('tr')).appendTo(tbody);
-	var col_heads = ['rule_id', 'subject', 'predicate','object', 'created_on', 'created_by','action'];
+	var extraTr = $(document.createElement('tr')).appendTo(tbody);
+	var col_heads = ['rule_id', 'subject', 'predicate','object', 'validation','notes','created_on', 'created_by','action'];
 	
 	// now create the input fields for adding stuff
 	var input = { 
 				'rule_id' : '',
 				'subject' : $(document.createElement('select')).attr('id', 'subject_select'),
-				'predicate' : $(document.createElement('select')).attr('id', 'predicate_select'),
-				'object' : $(document.createElement('select')).attr('id', 'object_select'),
+				'predicate' : $(document.createElement('select')).attr('id', 'predicate_select').change( predicateSelected ),
+				'object' : $(document.createElement('select')).attr('id', 'object_select').change ( objectSelected ),
+				'validation':$(document.createElement('input')).attr('id', 'validation').attr('type','text').attr('value','UID').attr("disabled", true),
+				'notes':$(document.createElement('input')).attr('id', 'notes').attr('type','text'),
 				'created_on' : '',
 				'created_by' : '',
-				'action' : ''
+				'action' : $(document.createElement('input')).attr('id', 'create_rule').attr('type','button').attr('value','Create')
 				};
 
 	for (var i=0; i<col_heads.length; i++) {
 		var required = (col_heads[i].match(/subject|predicate|object/))?'<sup>*</sup>':'';
 		$(document.createElement('th')).attr('id', 'col_head_'+col_heads[i]).html(col_heads[i]+required).appendTo(thtr);
-		$(document.createElement('th')).attr('id', col_heads[i]+'_input').append(input[col_heads[i]]).appendTo(inputTr);
+		$(document.createElement('td')).attr('id', col_heads[i]+'_input').append(input[col_heads[i]]).appendTo(inputTr);
+		$(document.createElement('td')).attr('id', col_heads[i]+'_extra').appendTo(extraTr);
 	}
 	
 	
@@ -113,9 +117,7 @@ function newRuleTable() {
 	$('#object_select').append('<img src="loading.gif">');
 	
 	//for predicate, tehre are other 3 options: New, from Bioontology, From another collection
-	$('#predicate_select').append($(document.createElement('option')).attr('value','new').html('<i>(New)</i>'))
-		.append($(document.createElement('option')).attr('value','ontology').html('<i>(Select from a Bio-Ontology!)</i>'))
-		.append($(document.createElement('option')).attr('value','other').html('<i>(Item_id from another collection)</i>'));
+	$('#predicate_select').append('<img src="loading.gif">');
 	
 	search_collection(GET['project_id'], ['subject_select', 'object_select']);
 	//search_predicate(s3dbVerbID); this guy is being search right after retrtieving collection s3dbVerb
@@ -132,7 +134,7 @@ function search_collection(project_id, select_holder) {
 		var select_holder = [select_holder];
 	}
 	if(s3dbCollections.length==0 && !collectionsCalled){
-		if(typeof(project_id)!=='undefined'){
+		if(typeof(project_id)!=='undefined' && project_id!==''){
 		  var q = S3QLtranslator('C|P'+project_id);
 		  
 		}
@@ -141,7 +143,7 @@ function search_collection(project_id, select_holder) {
 		}
 		
 		var url2call = GET['url']+"/S3QL.php?key="+GET['key']+"&query="+q+"&format=json";
-		  // s3dbcall(url,"result=ans;alert(':-)')","result=0;alert(':-(')");
+		  
 		  $.getJSON(url2call+'&callback=?', function (collections) {
 			collectionsCalled = true;
 			s3dbCollections = collections;
@@ -149,7 +151,7 @@ function search_collection(project_id, select_holder) {
 			//as the collections come along, fill out the options on collection_select
 			for (var s=0; s<select_holder.length; s++) {
 				$('#'+select_holder[s]).html('');
-				appendToSelect(collections, select_holder[s]);
+				appendToSelectC(collections, select_holder[s]);
 			}
 			
 			
@@ -158,12 +160,12 @@ function search_collection(project_id, select_holder) {
 	}
 	else {
 		for (var s=0; s<select_holder.length; s++) {
-				appendToSelect(collections, select_holder[s]);
+				appendToSelectC(collections, select_holder[s]);
 			}
 	}
 }
 
-function appendToSelect(data, select_holder) {
+function appendToSelectC(data, select_holder) {
 	for (var i=0; i<data.length; i++) {
 				if(data[i].name=='s3dbVerb') { 
 					s3dbVerbID = data[i].collection_id; 
@@ -171,6 +173,26 @@ function appendToSelect(data, select_holder) {
 				}
 				$(document.createElement('option')).attr('value', data[i].collection_id).html(data[i].name+' (C'+data[i].collection_id+')').appendTo($('#'+select_holder));
 			}
+	if(select_holder==='object_select'){
+			$(document.createElement('option')).attr('value', 'new').html('<i>(New)</i>').appendTo($('#'+select_holder));	
+			$(document.createElement('option')).attr('value', 'all').html('<i>(View all collections)</i>').appendTo($('#'+select_holder));
+			$(document.createElement('option')).attr('value', 'project_col').html('<i>(View only project collections)</i>').appendTo($('#'+select_holder));	
+	}	
+}
+
+function appendToSelectP(data, select_holder) {
+	$('#'+select_holder).html('');
+		//as the items  come along, fill out the options on collection_select
+		for (var i=0; i<data.length; i++) {
+			if(data[i].notes!=='')
+			$(document.createElement('option')).attr('value', data[i].item_id).html(data[i].notes+' (I'+data[i].item_id+')').appendTo($('#'+select_holder));
+		}
+		
+		//for predicate, there are other 3 options: New, from Bioontology, From another collection
+		$('#'+select_holder).append($(document.createElement('option')).attr('value','new').html('<i>(New)</i>'))
+			.append($(document.createElement('option')).attr('value','ontology').html('<i>(Select from a Bio-Ontology!)</i>'))
+			.append($(document.createElement('option')).attr('value','other').html('<i>(Item_id from another collection)</i>'));
+
 }
 
 function search_predicate(s3dbVerbID) {
@@ -184,12 +206,7 @@ function search_predicate(s3dbVerbID) {
 		var url2call = GET['url']+"/S3QL.php?key="+GET['key']+"&query="+q+"&format=json";
 		   $.getJSON(url2call+'&callback=?', function (items) {
 			s3dbVerbs = items;
-			$('#predicate_select').html('');
-			//as the collections come along, fill out the options on collection_select
-			for (var i=0; i<items.length; i++) {
-				if(items[i].notes!=='')
-				$(document.createElement('option')).attr('value', items[i].item_id).html(items[i].notes+' (I'+items[i].item_id+')').appendTo($('#predicate_select'));
-			}
+			appendToSelectP(items, 'predicate_select');
 			
 		  });
 	}
@@ -242,6 +259,110 @@ function s3db_edit(id, E) {
 
 function s3db_delete(id, E) {
 	console.log('delete '+rule_id);
+}
+
+function  predicateSelected() {
+	var predicate_id = document.getElementById('predicate_select');
+	
+	var selected = predicate_id.options[predicate_id.selectedIndex].value;
+	if (selected=='new') {
+		
+		//document.getElementById('predicate_input').innerHTML = '<input type="text" name="verb" id="verb"><input type="button" name="edit_item_verb" value="Choose from Items" onClick="window.location=window.location.href.replace(\'literal_verb=1\',\'\').replace(\'item_verb=0\',\'\')">';
+		
+		if($('#verb').length==0){
+		$('#predicate_select').hide();
+		$('#predicate_input').append($(document.createElement('input')).attr('type', 'text').attr('id', 'verb'));
+		$('#predicate_extra').append($(document.createElement('input')).attr('type', 'button').attr('value', 'Choose from Items').click ( function () {
+								$('#predicate_select').show();  $('#verb').hide();
+							}
+							));
+		}
+		else {
+			$('#predicate_select').hide();  $('#verb').show();
+		}
+
+	}
+	else if(selected=='other'){
+		//window.location = window.location.href.replace('literal_verb=1','').replace('item_verb=0','') +'&any_item=1';
+		if($('#verb_id').length==0){
+		 $('#predicate_select').hide();
+		 $('#predicate_input').append($(document.createElement('input')).attr('type', 'text').attr('id', 'verb_id'))
+		 $('#predicate_extra').append($(document.createElement('input')).attr('type', 'button').attr('value', 'Choose from Items').click ( function () {
+								$('#predicate_select').show();  $('#verb_id').hide();
+							}
+							));
+		}
+		else {
+			$('#predicate_select').hide();  $('#verb_id').show();
+		}
+
+	}
+	else if(selected=='ontology'){
+		
+		//document.getElementById('predicate_input').innerHTML = '<input type="text" name="verb" id="verb" class="bp_form_complete-all-name">Type 3 or more letters<input type="button" name="edit_item_verb" value="Choose from Items" onClick="window.location=window.location.href.replace(\'literal_verb=1\',\'\').replace(\'item_verb=0\',\'\')">';
+		if($('#verb').length==0){
+		$('#predicate_select').hide();
+		$('#predicate_input').append($(document.createElement('input')).attr('type', 'text').attr('id', 'verb').attr('class', 'bp_form_complete'))
+		$('#predicate_extra').append('<div>Type 3 or more letters</div>')
+							.append($(document.createElement('input')).attr('type', 'button').attr('value', 'Choose from Items').click ( function () {
+								$('#predicate_select').show();  $('#verb').hide();
+							}
+							));
+		}
+		else {
+			$('#predicate_select').hide();  $('#verb').show();
+		}
+
+		// Grab the specific scripts we need and fires it start event
+		jQuery.getScript("http://bioportal.bioontology.org/javascripts/JqueryPlugins/autocomplete/crossdomain_autocomplete.js", function(){
+			formComplete_setup_functions();
+		});
+		
+		
+		
+	}
+}
+
+function objectSelected()
+{
+	var obj_id = document.getElementById('object_select');
+	
+	var selected = obj_id.options[obj_id.selectedIndex].value;
+	if (selected=='new') {
+		if($('#object').length==0){
+			$('#object_select').hide(); 
+			$('#object_input').append($(document.createElement('input')).attr('type','text').attr('id','object'));
+			$('#object_extra').append($(document.createElement('input')).attr('type','button').attr('id','object').attr('value', 'Choose from Collections').click (function () {
+				$('#object_select').show();  $('#object').hide();
+				$('#validation').attr('disabled', true).attr('value','UID');
+			}))
+		}
+		else {
+			$('#object_select').hide();  $('#object').show();	
+		}
+		
+		$('#validation').removeAttr('disabled').attr('value','');
+		
+		
+
+	}
+	else if(selected=='all'){
+		$('#validation').attr('disabled', true).attr('value','UID');
+		$('#object_select').append('<img src="loading.gif"></img>');
+		search_collection('', 'object_select');//search without project
+		
+		//window.location = window.location.href +'&any_class=1';
+		//now for validation field
+		//document.getElementById('validation').value = 'UID';
+		//document.getElementById('validation').disabled = true;
+	}
+	else if(selected=='project_col') {
+		
+		window.location = window.location.href.replace('&any_class=1','&any_class=0');
+		//now for validation field, remove "UID" and allow any validation
+		document.getElementById('validation').value = 'UID';
+		document.getElementById('validation').disabled = true;
+	}
 }
 
 function include(src, path) {
